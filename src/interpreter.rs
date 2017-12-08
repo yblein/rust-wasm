d66c89cb1750689fcdaf2633209d18180a0d2fab
Yoann Blein <yoann.blein@free.fr>
2017-12-08 16:39:08 +0100

    fix reinterpret

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -540,7 +540,7 @@ impl Interpreter {
 
 				(&ConvertOp::F32DemoteF64, Value::F64(c)) => Value::F32(c.demote()),
 				(&ConvertOp::F64PromoteF32, Value::F32(c)) => Value::F64(c.promote()),
-				_ => return None // FIXME: unreachable!()
+				_ => unreachable!()
 			})
 		};
 
